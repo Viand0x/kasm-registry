2bb83a60ee4c17499424461568fdd1d7c4df3758
Viand0x <j.doe@fifetxav.net>
2026-01-12 10:16:53 +0100

    Update listUrl in next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
     name: 'Kasm Technologies',
     description: 'The official store for Kasm supported workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://viand0x.github.io/',
+    listUrl: 'https://viand0x.github.io/kasm-registry/',
     contactUrl: 'https://kasmweb.com/support',
   },
   reactStrictMode: true,
