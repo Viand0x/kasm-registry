06bcc2bd21cea6d1539744b0b1bd218b3ba82c1a
Viand0x <j.doe@fifetxav.net>
2026-01-12 10:08:54 +0100

    Update listUrl to new GitHub Pages URL

--- site/next.config.js
+++ site/next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
     name: 'Kasm Technologies',
     description: 'The official store for Kasm supported workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
+    listUrl: 'https://viand0x.github.io/',
     contactUrl: 'https://kasmweb.com/support',
   },
   reactStrictMode: true,
